9e6cd36e8ef6921a9cc7877ae70da91720fe1f59
watcharapon.kes <thecvit2328@gmail.com>
2019-11-29 16:34:46 +0700

    add noble fromline function

--- src/main/kotlin/domain/Noble.kt
+++ src/main/kotlin/domain/Noble.kt
@@ -1,3 +1,20 @@
 package domain
+import java.lang.IllegalArgumentException
 
-class Noble(val score: Int, val prices: Map<Gem, Int>)
+class Noble(val score: Int, val prices: Map<Card, Int>){
+    companion object{
+        @JvmStatic
+        fun fromLine(line: String): Noble {
+            val column = line.split("|")
+            val listPrice = column[1].substring(1, column[1].length - 1).split(",")
+            val price = mutableMapOf({
+                Gem.WHITE to listPrice[0].toInt(),
+                Gem.BLUE to listPrice[1].toInt(),
+                Gem.GREEN to listPrice[2].toInt(),
+                Gem.RED to listPrice[3].toInt(),
+                Gem.BLACK to listPrice[4].toInt()
+            }).filterValues { it != 0 }
+            return Noble(column[0].toInt(),price)
+        }
+    }
+}
